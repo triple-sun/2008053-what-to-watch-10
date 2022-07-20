8dc3b33a1dc35fa8250a7557f5f3c843bff99752
triple-sun <sploit.strannik@gmail.com>
2022-07-20 23:53:24 +0300

    Fixed Private Route

--- project/src/components/common/private-route/private-route.tsx
+++ project/src/components/common/private-route/private-route.tsx
@@ -1,15 +1,14 @@
-import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../../const/enums';
 
 type PrivateRouteProps = {
   authorizationStatus: AuthorizationStatus;
-  children: ReactNode;
+  children: JSX.Element;
 }
 
 const PrivateRoute = ({authorizationStatus, children}: PrivateRouteProps) => (
   authorizationStatus === AuthorizationStatus.Auth
-    ? <>{children}</>
+    ? children
     : <Navigate to={AppRoute.Login} />
 );
 
