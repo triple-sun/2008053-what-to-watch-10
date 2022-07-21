4bdc29d038fb4de52482a62f3f23431e3286c164
triple-sun <sploit.strannik@gmail.com>
2022-07-21 17:34:28 +0300

    Minor fixes

--- project/src/components/movie-player/player-buttons/exit-player-button/exit-player-button.tsx
+++ project/src/components/movie-player/player-buttons/exit-player-button/exit-player-button.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../../../const/enums';
 
 const ExitPlayerButton = () => {
