730272882783346500461e21e8f5b95353d7040f
triple-sun <sploit.strannik@gmail.com>
2022-07-29 11:23:56 +0300

    Removed unnecessary types

--- project/src/utils/selectors/selectors.ts
+++ project/src/utils/selectors/selectors.ts
@@ -1,10 +1,7 @@
-import { Genre } from '../../const/enums';
 import State from '../../types/state';
 
 export const getMovies = (state: State) => state.movies;
 
 export const getPromo = (state: State) => state.promo;
-
-export const getMoviesByGenre = (state: State) => state.selectedGenre === Genre.AllGenres ? state.movies : state.movies.filter((movie) => movie.genre === state.selectedGenre);
 
 export const getSelectedGenre = (state: State) => state.selectedGenre;
