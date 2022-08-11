abdee30d08b0c705c61f9b912e7e94e7f0ece0cb
triple-sun <sploit.strannik@gmail.com>
2022-08-12 00:16:38 +0300

    Refactored pages and components

--- project/src/components/my-list-title/my-list-title.tsx
+++ project/src/components/my-list-title/my-list-title.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
+
 const MyListTitle = ({count}: {count: number}) => <h1 className="page-title user-page__title">My list <span className="user-page__film-count">{count}</span></h1>;
 
-export default MyListTitle;
+export default React.memo(MyListTitle);
 
